71c32abd5f55605f754887ef23c978af7dbd2701
reznord <allamsetty.anup@gmail.com>
2017-04-05 12:00:07 +0530

    replaced browserHistroy with hashHistory

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import routes from './routes';
 
 /* global css */
 import './index.css';
 
 render(
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     {routes}
   </Router>, document.getElementById('root'),
 );
